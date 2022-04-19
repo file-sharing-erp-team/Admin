33097de23668a1a4edaa6038f27b1cd3093ad94a
Mikhail Toropchinov <46166975+ruhose73@users.noreply.github.com>
2022-04-19 23:26:28 +0300

    гзвфеу

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -7,7 +7,19 @@ interface LoaderProps {
 const Loader: React.FC<LoaderProps> = ({ isLoading}) => {
     return (
         <div>
-            ЗАГРУЗКА!!!
+            <div className="preloader-wrapper big active">
+                <div className="spinner-layer spinner-blue">
+                    <div className="circle-clipper left">
+                        <div className="circle"></div>
+                    </div>
+                    <div className="gap-patch">
+                        <div className="circle"></div>
+                    </div>
+                    <div className="circle-clipper right">
+                        <div className="circle"></div>
+                    </div>
+                </div>
+        </div>
         </div>
     );
 };
